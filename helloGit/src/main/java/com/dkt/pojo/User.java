3c24cd9d3a57b5cc84250f87ca47262eb2198ae3
zhangyanyv <2753779669@qq.com>
2021-12-29 20:12:51 +0800

    第一次提交

---
+++ helloGit/src/main/java/com/dkt/pojo/User.java
@@ -0,0 +1,26 @@
+package com.dkt.pojo;
+
+/**
+ * @Author: dkt
+ * @Date: 2021/12/29 17:57
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername () {
+        return username;
+    }
+
+    public void setUsername (String username) {
+        this.username = username;
+    }
+
+    public String getPassword () {
+        return password;
+    }
+
+    public void setPassword (String password) {
+        this.password = password;
+    }
+}
